2e2c7098769b0d47108892b9ef49a3a8dd003686
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-07 23:12:43 +0200

    App - import Home Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import Directory from "../../components/Directory/Directory";
+
 import "./HomePage.scss";
 
-const HomePage = () => <div className="homepage"></div>;
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
 
 export default HomePage;
