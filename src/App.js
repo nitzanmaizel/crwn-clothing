e5ce30bd461e5257eb2343b0827a97eac03963f9
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-08 01:02:15 +0200

    App - render shopPage as a route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+
 import HomePage from './pages/HomePage/HomePage';
+import ShopPage from './pages/ShopPage/ShopPage';
 
 const App = () => {
   return (
     <div>
       <Route exact path={'/'} component={HomePage} />
+      <Route exact path={'/shop'} component={ShopPage} />
     </div>
   );
 };
