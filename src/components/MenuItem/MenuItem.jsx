3c2336e9888624e84104b556c6aa84f92aa3dcf3
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-07 23:10:34 +0200

    MenuItem - add style and pass props

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const MenuItem = () => {
-  return <div>MenuItem</div>;
-};
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
 
 export default MenuItem;
