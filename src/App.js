545deb02337d1d19c4d5aac80014208e79dff7e3
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-08 01:52:59 +0200

    App - render AuthPage as a route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
+import AuthPage from './pages/AuthPage/AuthPage';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Switch>
         <Route exact path={'/'} component={HomePage} />
         <Route path={'/shop'} component={ShopPage} />
+        <Route path={'/auth'} component={AuthPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './AuthPage.scss';
+
+const AuthPage = () => {
+  return <div className='authContainer'>AuthPage</div>;
+};
+
+export default AuthPage;
