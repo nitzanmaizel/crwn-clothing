48aa6be7af79f47affa0ce2a44a9ea550326d27e
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-08 00:09:43 +0200

    App - render HomePage as Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 
 const App = () => {
   return (
     <div>
-      <HomePage />
+      <Route exact path={'/'} component={HomePage} />
     </div>
   );
 };
