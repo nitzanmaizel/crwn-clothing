a118bf0d0c3b7d0151923040d26713e0fd3f6112
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-08 01:57:09 +0200

    SignIn - add input for email and psw, add button and create form state

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const { signInForm, setSignInForm } = useState({ email: '', password: '' });
+  return (
+    <div className='sign-in'>
+      <h2>I already have an account</h2>
+      <span>Sign in with your email and password</span>
+
+      <form onSubmit={() => {}}>
+        <input
+          name='email'
+          type='email'
+          onChange={() => {}}
+          value={signInForm.email}
+          label='email'
+          required
+        />
+        <input
+          name='password'
+          type='password'
+          value={signInForm.password}
+          onChange={() => {}}
+          label='password'
+          required
+        />
+        <button type='submit'> Sign in </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
