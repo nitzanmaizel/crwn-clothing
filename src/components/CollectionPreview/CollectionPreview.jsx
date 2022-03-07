f5176a4677b3da8b86c33b276fcaf5ae9d984345
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-08 00:58:01 +0200

    CollectionPreview - filter and map the collection

--- src/components/CollectionPreview/CollectionPreview.jsx
+++ src/components/CollectionPreview/CollectionPreview.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const CollectionPreview = () => {
-  return <div>CollectionPreview</div>;
+const CollectionPreview = ({ title, collection }) => {
+  return (
+    <div className='collectionPreview'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <div className='preview'>
+        {collection.length &&
+          collection.filter((_, idx) => idx < 4).map(({ id, ...otherCollectionProps }) => <div />)}
+      </div>
+    </div>
+  );
 };
 
 export default CollectionPreview;
