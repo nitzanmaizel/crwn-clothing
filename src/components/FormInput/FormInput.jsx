f069c06a9e3d5d52c3718c17b31ad8ecb02b66a6
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-08 02:17:29 +0200

    FormInput - import styles

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './FormInput.scss';
+
 const FormInput = ({ handleChange, value, label, ...otherProps }) => (
   <div className='group'>
     <input className='form-input' onChange={handleChange} {...otherProps} />
