460a2fef730e297966317f07ac7612398888c82c
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-08 22:15:25 +0200

    App - listen om useEffact when use is signIn with google and update user state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
 import AuthPage from './pages/AuthPage/AuthPage';
 import Header from './components/Header/Header';
+import { auth } from './firebase/firebase';
 
 const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+      }
+    });
+
+    return () => unsubscribeFromAuth();
+  }, []);
+
   return (
     <div>
       <Header />
