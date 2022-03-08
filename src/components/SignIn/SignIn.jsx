0aa8d5b15d2cb0bdedff07b1d542198265462f3d
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-08 02:03:07 +0200

    AuthPage - import signIn form

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,25 +1,21 @@
 import React, { useState } from 'react';
 
 const SignIn = () => {
-  const { signInForm, setSignInForm } = useState({ email: '', password: '' });
+  const [signInForm, setSignInForm] = useState({ email: '', password: '' });
+
+  const { email, password } = signInForm;
+
   return (
     <div className='sign-in'>
       <h2>I already have an account</h2>
       <span>Sign in with your email and password</span>
 
       <form onSubmit={() => {}}>
-        <input
-          name='email'
-          type='email'
-          onChange={() => {}}
-          value={signInForm.email}
-          label='email'
-          required
-        />
+        <input name='email' type='email' onChange={() => {}} value={email} label='email' required />
         <input
           name='password'
           type='password'
-          value={signInForm.password}
+          value={password}
           onChange={() => {}}
           label='password'
           required
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import SignIn from '../../components/SignIn/SignIn';
 
 import './AuthPage.scss';
 
 const AuthPage = () => {
-  return <div className='authContainer'>AuthPage</div>;
+  return (
+    <div className='authContainer'>
+      <SignIn />
+    </div>
+  );
 };
 
 export default AuthPage;
