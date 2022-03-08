d2687298f3f51f8877a85cccf7e40def0dc9c083
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-08 23:55:56 +0200

    SignUp - create form with FormInput and CustonButtom components

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import CustomButton from '../CustomButton/CustomButton';
+import FormInput from '../FormInput/FormInput';
 
 import './SignUp.scss';
 
@@ -10,7 +12,50 @@ const SignUp = () => {
     confirmPassword: '',
   });
 
-  return <div>SignUp</div>;
+  const { displayName, email, password, confirmPassword } = signUpForm;
+
+  return (
+    <div className='signUp'>
+      <h2 className='title'>I do not have a account</h2>
+      <span>Sign up with your email and password</span>
+
+      <form className='signUpForm' onSubmit={() => {}}>
+        <FormInput
+          type='text'
+          name='displayName'
+          value={displayName}
+          onChange={() => {}}
+          label='Display Name'
+          required
+        />
+        <FormInput
+          type='email'
+          name='email'
+          value={email}
+          onChange={() => {}}
+          label='Email'
+          required
+        />
+        <FormInput
+          type='password'
+          name='password'
+          value={password}
+          onChange={() => {}}
+          label='Password'
+          required
+        />
+        <FormInput
+          type='password'
+          name='confirmPassword'
+          value={confirmPassword}
+          onChange={() => {}}
+          label='Confirm Password'
+          required
+        />
+        <CustomButton title='SIGN UP' type='submit' />
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
