afb77c43ba336ee3279e9d6aad5a8bb8f1ef8df2
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-09 23:27:57 +0200

    CartMenu - render icon and a span with item count

--- src/components/CartMenu/CartMenu.jsx
+++ src/components/CartMenu/CartMenu.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
 import './CartMenu.scss';
 
 const CartMenu = () => {
-  return <div>CartMenu</div>;
+  return (
+    <div className='cartMenu'>
+      <ShoppingIcon className='shoppingIcon' />
+      <span className='itemCount'>0</span>
+    </div>
+  );
 };
 
 export default CartMenu;
