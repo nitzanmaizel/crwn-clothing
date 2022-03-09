462f3f7ee4107b7ef541163b74abc91fe3e333c6
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-09 22:30:09 +0200

    Redux - create folder and relevent files to the user actions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,9 +11,11 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
+  // <Provider>
   <BrowserRouter>
     <App />
   </BrowserRouter>,
+  // </Provider>
   document.getElementById('root'),
 );
 
