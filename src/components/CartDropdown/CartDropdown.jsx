5c49b37b736082b63cf896ddaef6c2a36e7e92fa
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-10 01:12:56 +0200

    CartDropDwon - connect component to state and get the cartItems state as a props

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../CustomButton/CustomButton';
 
 import './CartDropdown.scss';
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className='cartDropdown'>
       <div className='cartItems' />
@@ -13,4 +14,8 @@ const CartDropdown = () => {
   );
 };
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
