4bd1cc3f78737762f74b871687ad48d550da7462
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-09 23:35:54 +0200

    CartDropdown - render continer and vutton

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import CustomButton from '../CustomButton/CustomButton';
+
+import './CartDropdown.scss';
+
 const CartDropdown = () => {
-  return <div>CartDropdown</div>;
+  return (
+    <div className='cartDropdown'>
+      <div className='cartItems' />
+      <CustomButton title='GO TO CHECKOUT' />
+    </div>
+  );
 };
 
 export default CartDropdown;
