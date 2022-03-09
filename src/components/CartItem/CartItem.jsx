0dfcb94cba7a6a4f3789ac4f38c8dfea6862d982
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-10 01:09:31 +0200

    CartItem - render img, price and quantity

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+const CartItem = ({ item }) => {
+  const { imageUrl, price, name, quantity } = item;
+
+  return (
+    <div className='cart-item'>
+      <img src={imageUrl} alt='item' />
+      <div className='item-details'>
+        <span className='name'>{name}</span>
+        <span className='price'>
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
