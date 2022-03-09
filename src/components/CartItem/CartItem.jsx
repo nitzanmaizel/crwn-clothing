3696482e757272a2c0130056d455174785736c5a
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-10 01:10:31 +0200

    CartItem - add styles

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import './CartItem.scss';
+
 const CartItem = ({ item }) => {
   const { imageUrl, price, name, quantity } = item;
 
   return (
-    <div className='cart-item'>
-      <img src={imageUrl} alt='item' />
-      <div className='item-details'>
+    <div className='cartItem'>
+      <img src={imageUrl} alt={name} />
+      <div className='itemDetails'>
         <span className='name'>{name}</span>
         <span className='price'>
           {quantity} x ${price}
