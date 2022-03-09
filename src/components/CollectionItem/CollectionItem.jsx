d9c37913828d886b5d3c73603eb570d3a4cca1be
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-10 00:53:02 +0200

    CollectionItem - destrctere item prop

--- src/components/CollectionItem/CollectionItem.jsx
+++ src/components/CollectionItem/CollectionItem.jsx
@@ -3,20 +3,23 @@ import CustomButton from '../CustomButton/CustomButton';
 
 import './CollectionItem.scss';
 
-const CollectionItem = ({ name, price, imageUrl }) => (
-  <div className='collectionItem'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>${price}</span>
+const CollectionItem = ({ item }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className='collectionItem'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>${price}</span>
+      </div>
+      <CustomButton inverted title='Add to cart' />
     </div>
-    <CustomButton inverted title='Add to cart' />
-  </div>
-);
+  );
+};
 
 export default CollectionItem;
