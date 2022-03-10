45e4686f821c02445fd708ce919ceb73afaaa78c
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-10 23:03:13 +0200

    CheckoutPage - call selectCartTotal selctor and add to props total - show total price

--- src/pages/CheckOutPage/CheckOutPage.jsx
+++ src/pages/CheckOutPage/CheckOutPage.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCartItems } from '../../redux/cart/cartSelectors';
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cartSelectors';
 
 import CheckoutHeader from './CheckoutHeader';
 import CheckoutItem from '../../components/CheckoutItem/CheckoutItem';
 
 import './CheckOutPage.scss';
 
-const CheckOutPage = ({ cartItems }) => {
+const CheckOutPage = ({ cartItems, total }) => {
   return (
     <div className='checkoutPage'>
       <CheckoutHeader />
@@ -18,13 +18,14 @@ const CheckOutPage = ({ cartItems }) => {
       ) : (
         <div className='emptyMessage'>Your cart is empty</div>
       )}
-      <div className='total'>TOTAL: $0</div>
+      <div className='total'>TOTAL: ${total}</div>
     </div>
   );
 };
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
+  total: selectCartTotal,
 });
 
 export default connect(mapStateToProps)(CheckOutPage);
