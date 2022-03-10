6491fd560a11cc8df18c937a3432026382a310dc
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-11 01:04:28 +0200

    Directory - connect to Redux state and get the directory state

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -1,22 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directorySelectors';
+
 import MenuItem from '../MenuItem/MenuItem';
-import ListMenuItems from '../../redux/directory/directoryState';
 
 import './Directory.scss';
 
-const Directory = () => {
-  const [section, setSection] = useState([]);
-
-  useEffect(() => {
-    setSection(ListMenuItems);
-  }, []);
-
+const Directory = ({ sections }) => {
   return (
     <div className='directory-menu'>
-      {section.length &&
-        section.map(({ id, ...OtherSectionProps }) => <MenuItem key={id} {...OtherSectionProps} />)}
+      {sections.length &&
+        sections.map(({ id, ...OtherSectionProps }) => (
+          <MenuItem key={id} {...OtherSectionProps} />
+        ))}
     </div>
   );
 };
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
+
+export default connect(mapStateToProps)(Directory);
