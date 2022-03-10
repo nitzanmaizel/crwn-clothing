778e3e62603a8c7efea9fb051ed4d24e15d465bd
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-10 22:42:17 +0200

    CheckoutItem - render jsx -  item image, price, quantity, and remove icon

--- src/components/CheckoutItem/CheckoutItem.jsx
+++ src/components/CheckoutItem/CheckoutItem.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const CheckoutItem = () => {
-  return <div>CheckoutItem</div>;
+const CheckoutItem = ({ item }) => {
+  const { name, imageUrl, price, quantity } = item;
+  \
+  return (
+    <div className='checkoutItem'>
+      <div className='imageContainer'>
+        <img src={imageUrl} alt={name} />
+      </div>
+      <span className='name'>{name}</span>
+      <span className='quantity'>{quantity}</span>
+      <span className='price'>{price}</span>
+      <div className='removeButton'>&#10005;</div>
+    </div>
+  );
 };
 
 export default CheckoutItem;
