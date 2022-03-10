c6a82ccdd0b52a1844d88a7bb228ff0495e34f65
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-10 22:21:01 +0200

    CheckOutPage - render jsx with header tabs and total price

--- src/pages/CheckOutPage/CheckOutPage.jsx
+++ src/pages/CheckOutPage/CheckOutPage.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
 const CheckOutPage = () => {
-  return <div>CheckOutPage</div>;
+  return (
+    <div className='checkoutPage'>
+      <div className='checkoutHeader'>
+        <div className='headerTab'>
+          <span>Product</span>
+        </div>
+        <div className='headerTab'>
+          <span>Description</span>
+        </div>
+        <div className='headerTab'>
+          <span>Quantity</span>
+        </div>
+        <div className='headerTab'>
+          <span>Price</span>
+        </div>
+        <div className='headerTab'>
+          <span>Remove</span>
+        </div>
+      </div>
+      <div className='total'>TOTAL: $0</div>
+    </div>
+  );
 };
 
 export default CheckOutPage;
