9ce71fdd2a9638d243e6510f1ee285b634d642aa
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-10 23:31:07 +0200

    Redux -cart - create fun clearItemFromCart that fillter the item from itemsCart

--- src/redux/cart/cartUtils.js
+++ src/redux/cart/cartUtils.js
@@ -11,3 +11,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const clearItemFromCart = (cartItems, cartItemToClear) => {
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+};
