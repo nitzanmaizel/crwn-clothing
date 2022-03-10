0924a52bbf76b3bdcf17af54d64f3375d74089fa
Nitzan Maizel <nitzan.maizel@zoominsoftware.com>
2022-03-11 00:19:47 +0200

    Index - wrapp App with PersistGate

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
